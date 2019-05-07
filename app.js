a7bf95541fd40fa08a4412bfb7eaff871277a976
Jejo <jejomar@im.skubbs.com>
2019-05-07 21:46:49 +0800

    added validation for winning case

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-alert('jejo')
+//game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+//assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+//listen for guess
+guessBtn.addEventListener('click', function() {
+    let guess = parseInt(guessInput.value);
+
+    //validate
+    if ( isNaN(guess) || guess < min || guess > max ) {
+        setMessage(`Please enter a number between ${min} and ${max}.`, 'red');
+    }
+
+    //check if won
+    if ( guess === winningNum ) {
+        //disable input
+        guessInput.disabled = true;
+        guessInput.style.borderColor = 'green';
+        setMessage(`${winningNum} is correct, you win!`, 'green');
+    } else {
+
+    }
+});
+
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+}
